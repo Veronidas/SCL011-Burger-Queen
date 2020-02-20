50e3ccd462e8aca8288f91c826694a1512bb794e
veronidas <veronabrkich@gmail.com>
2020-02-20 13:03:14 -0300

    creacion vistas breakfast y lunch, integradas a la app

---
+++ src/views/breakfast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Breakfast extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Desayunos!!</h1>
+            </div>
+        )
+    }
+}
+
+export default Breakfast;
---
+++ src/views/lunch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Lunch extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Almuerzos!</h1>
+            </div>
+        )
+    }
+}
+
+export default Lunch;
