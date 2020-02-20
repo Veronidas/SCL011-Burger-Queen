9b6b5781b5380b18a3478c7d521b1c5684a4ee0f
veronidas <veronabrkich@gmail.com>
2020-02-20 13:26:20 -0300

    creacion primer componente botonenviar e integracion con la vista desayuno

---
+++ src/components/botonenviar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Enviar extends React.Component {
+    render() {
+        return (
+            <button>Enviar</button>
+        )
+    }
+}
+
+export default Enviar;
