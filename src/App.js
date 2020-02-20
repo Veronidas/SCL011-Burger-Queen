e2d89a6b90a68d05e5fd3aed14bfa27e72166623
veronidas <veronabrkich@gmail.com>
2020-02-20 13:16:05 -0300

    routing a todas las vistas, wrap de la app en browserrouting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './views/home'
+import Breakfast from './views/breakfast';
+import Lunch from './views/lunch';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="App">.
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/desayuno" component={Breakfast} />
+        <Route exact path="/almuerzo" component={Lunch} />
+      </Switch>
     </div>
   );
 }
