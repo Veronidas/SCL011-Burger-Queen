3c56263d40a163cf32e7c320b70303c8c14e1ef0
veronidas <veronabrkich@gmail.com>
2020-02-20 13:22:42 -0300

    navbar added in routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
 import Home from './views/home'
 import Breakfast from './views/breakfast';
@@ -8,13 +8,18 @@ import Lunch from './views/lunch';
 
 function App() {
   return (
-    <div className="App">.
+    <div className="App">
+      <nav>
+        <NavLink exact activeClassName="active-link" to="/">  Home  </NavLink>
+        <NavLink exact activeClassName="active-link" to="/desayuno">  Desayunos  </NavLink>
+        <NavLink exact activeClassName="active-link" to="/almuerzo"> Almuerzos </NavLink>
+      </nav>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/desayuno" component={Breakfast} />
         <Route exact path="/almuerzo" component={Lunch} />
       </Switch>
-    </div>
+    </div >
   );
 }
 
