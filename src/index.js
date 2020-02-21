1517672b350bd2e3dec6e56136a9157669278fd6
veronidas <veronabrkich@gmail.com>
2020-02-21 04:01:15 -0300

    install firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+// import { createStore } from 'redux';
+
+// const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
