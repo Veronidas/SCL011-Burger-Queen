8f2bef43406ae99144f56e3a1bb2b7a8791ec05b
veronidas <veronabrkich@gmail.com>
2020-02-20 23:53:09 -0300

    creacion de el component ListItems, modificacion App.css

--- src/views/breakfast.js
+++ src/views/breakfast.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Enviar from '../components/botonenviar'
+import Menu from '../components/menuorden'
 
 class Breakfast extends React.Component {
     render() {
         return (
             <div className="App">
                 <h1>Desayunos!!</h1>
+                <Menu />
+                <Enviar />
             </div>
         )
     }
--- src/views/lunch.js
+++ src/views/lunch.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Menu from '../components/menuorden';
+import Enviar from '../components/botonenviar';
+
 
 class Lunch extends React.Component {
     render() {
         return (
             <div className="App">
                 <h1>Almuerzos!</h1>
+                <Menu />
+                <Enviar />
             </div>
         )
     }
