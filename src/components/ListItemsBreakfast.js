adeb656645cbed554d2f86ae617dce57d5041552
veronidas <veronabrkich@gmail.com>
2020-02-28 12:51:40 -0300

    funcion de conteo mejorada

--- src/components/ListItemsBreakfast.js
+++ src/components/ListItemsBreakfast.js
@@ -3,18 +3,17 @@ import data from './data/data'
 
 class ListItemsBreakfast extends React.Component {
     state = {
-        items: 0
+        count: 0
     }
-    handleClick = (e) => {
-        let counter = this.state.items + 1
-        this.setState({ items: counter });
+    handleClick = () => {
+        this.setState({ count: this.state.count + 1 });
     };
     render() {
         return (
             <div>
                 <button onClick={this.handleClick} className="btn pink accent-2">  +  </button>
                 <ul>{data.desayunos.map((data) => <li key={data.id}>{data.name} $ {data.price}</li>)}</ul>
-                <span>{this.state.items}</span>
+                <span>{this.state.count}</span>
             </div >
         )
     };
